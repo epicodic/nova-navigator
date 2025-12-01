2ebb6f212ffe6ec27c2a960c872fafd4002c5198
epicodic <epicodic@gmail.com>
2025-12-01 20:51:09 +0100

    refactor: removed obsolete code

--- src/nova_navigator/vfs2/__init__.py
+++ src/nova_navigator/vfs2/__init__.py
@@ -1,6 +1,5 @@
 from .filesystem import Filesystem
-from .interaction_context import InteractionContext
 from .types import Stat
 from .vpath import VPath
 
-__all__ = ["Filesystem", "InteractionContext", "Stat", "VPath"]
+__all__ = ["Filesystem", "Stat", "VPath"]
