1b0cb3a8deea961b04a70868f9b46c183106c76d
epicodic <epicodic@gmail.com>
2025-12-09 23:10:03 +0100

    wip

---
+++ src/nova_widgets/menu/__init__.py
@@ -0,0 +1,4 @@
+from ._menu import Menu, MenuItem
+from ._menu_bar import MenuBar
+
+__all__ = ["Menu", "MenuBar", "MenuItem"]
--- src/nova_widgets/menu/_menu_bar.py
+++ src/nova_widgets/menu/_menu_bar.py
@@ -1,13 +1,11 @@
-from rich.text import Text
 from textual import events, work
-from textual.app import ComposeResult, RenderResult
+from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.events import Enter, MouseDown
-from textual.geometry import Offset
 from textual.widget import Widget
 from textual.widgets import Static
 
-from .menu import Menu
+from ._menu import Menu
 
 
 class MenuBarItem(Static):
