d01c7c8149eeb76300573c9832c17f234b5a8314
epicodic <epicodic@gmail.com>
2026-03-28 15:40:45 +0100

    feat(widgets): add custom border widget

--- src/nova_widgets/__init__.py
+++ src/nova_widgets/__init__.py
@@ -1,5 +1,6 @@
 from .animated_icon import AnimatedIcon
+from .custom_border import CustomBorderMixin
 from .icon import Icon
 from .menu import Action, Menu, MenuBar
 
-__all__ = ["Action", "AnimatedIcon", "Icon", "Menu", "MenuBar"]
+__all__ = ["Action", "AnimatedIcon", "CustomBorderMixin", "Icon", "Menu", "MenuBar"]
