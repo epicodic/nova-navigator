e272fc176915925afed57076722485599295fe00
epicodic <epicodic@gmail.com>
2026-04-01 16:09:16 +0200

    refactor: export LocalFilesystem and SSHFilesystem from vfs2

--- src/nova_navigator/vfs2/__init__.py
+++ src/nova_navigator/vfs2/__init__.py
@@ -1,5 +1,7 @@
 from .filesystem import Filesystem
+from .filesystems.local import LocalFilesystem
+from .filesystems.ssh import SSHFilesystem
 from .types import Stat
 from .vpath import VPath
 
-__all__ = ["Filesystem", "Stat", "VPath"]
+__all__ = ["Filesystem", "LocalFilesystem", "SSHFilesystem", "Stat", "VPath"]
