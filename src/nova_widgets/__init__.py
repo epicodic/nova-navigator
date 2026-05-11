68a4b691c64fe16affa48dca26a092cf90aea5ee
epicodic <epicodic@gmail.com>
2026-05-11 17:36:37 +0200

    add ButtonBox

--- src/nova_widgets/__init__.py
+++ src/nova_widgets/__init__.py
@@ -1,6 +1,7 @@
 from .animated_icon import AnimatedIcon
+from .button_box import ButtonBox
 from .custom_border import CustomBorderMixin
 from .icon import Icon
 from .menu import Action, Menu, MenuBar
 
-__all__ = ["Action", "AnimatedIcon", "CustomBorderMixin", "Icon", "Menu", "MenuBar"]
+__all__ = ["Action", "AnimatedIcon", "ButtonBox", "CustomBorderMixin", "Icon", "Menu", "MenuBar"]
